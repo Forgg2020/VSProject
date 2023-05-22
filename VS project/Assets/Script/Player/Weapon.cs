a4777b2a0005856871fd6c791aba23979973ec69
Forgg2020 <billlin87823@gmail.com>
2023-05-22 15:28:03 +0900

    import asset

--- VS project/Assets/Script/Player/Weapon.cs
+++ VS project/Assets/Script/Player/Weapon.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    float attackFreq = 4f;
+    float timer;
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if(timer < 0f)
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("0.0");
+        timer = attackFreq;
     }
 }
