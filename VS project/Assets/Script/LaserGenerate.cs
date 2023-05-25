941bc1b935bbb1391eb1e61a8d9726bf5936f0cc
Forgg2020 <billlin87823@gmail.com>
2023-05-25 20:01:56 +0900

    add weapon audio, making laser

---
+++ VS project/Assets/Script/LaserGenerate.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGenerate : MonoBehaviour
+{
+    [SerializeField] public float defDistanceRay = 100;
+    public Transform laserFirePoint;
+    public LineRenderer m_LineRenderer;
+    Transform m_Transform;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_Transform = GetComponent<Transform>();
+    }
+    private void Update()
+    {
+        ShootLaser();
+    }
+
+    void ShootLaser()
+    {
+        if(Physics2D.Raycast(m_Transform.position,transform.right))
+        {
+            RaycastHit2D _hit = Physics2D.Raycast(laserFirePoint.position,transform.right);
+            Draw2DRay(laserFirePoint.position, _hit.point);
+        }else
+        {
+            Draw2DRay(laserFirePoint.position, laserFirePoint.transform.right * defDistanceRay);
+        }
+    }
+
+    void Draw2DRay(Vector2 startPos, Vector2 endPos)
+    {
+        m_LineRenderer.SetPosition(0, startPos);
+        m_LineRenderer.SetPosition(1, endPos);
+    }
+}
